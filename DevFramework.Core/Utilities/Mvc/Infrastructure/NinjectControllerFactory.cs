699e235bf3e0cd860478a6f960eac81bb620f045
Metin Çelebi <metincelebi5534@gmail.com>
2022-03-12 00:12:18 +0300

    WebApi implementations done. Auto Mapper implementation done. AutoMapper WebApi and  MVC implementation done.

--- DevFramework.Core/Utilities/Mvc/Infrastructure/NinjectControllerFactory.cs
+++ DevFramework.Core/Utilities/Mvc/Infrastructure/NinjectControllerFactory.cs
@@ -10,10 +10,11 @@ namespace DevFramework.Core.Utilities.Mvc.Infrastructure
     {
         IKernel _kernel;
 
-        public NinjectControllerFactory(INinjectModule module)
+        public NinjectControllerFactory(params INinjectModule[] module)
         {
             _kernel = new StandardKernel(module);
         }
+
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
             return controllerType == null
--- DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -1,4 +1,5 @@
-﻿using DevFramework.Core.Aspects.Postsharp.AuthorizationAspects;
+﻿using AutoMapper;
+using DevFramework.Core.Aspects.Postsharp.AuthorizationAspects;
 using DevFramework.Core.Aspects.Postsharp.CacheAspects;
 using DevFramework.Core.Aspects.Postsharp.PerformanceAspects;
 using DevFramework.Core.Aspects.Postsharp.TransactionAspects;
@@ -8,26 +9,27 @@ using DevFramework.Northwind.Business.ValidaitonRules.FluentValidation;
 using DevFramework.Northwind.DataAccess.Abstract;
 using DevFramework.Northwind.Entities.Concrete;
 using System.Collections.Generic;
-using System.Threading;
 
 namespace DevFramework.Northwind.Business.Concrete.Managers
 {
     public class ProductManager : IProductService
     {
         private IProductDal _productDal;
+        private readonly IMapper _mapper;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, IMapper mapper)
         {
             _productDal = productDal;
+            _mapper = mapper;
         }
 
         [CacheAspect(typeof(MemoryCacheManager))]
         [PerformanceCounterAspect(2)]
-        [SecuredOperation(Roles = "Admin,Editor,Student")]
+        //[SecuredOperation(Roles = "Admin,Editor,Student")]
         public List<Product> GetAll()
         {
-            //Thread.Sleep(3000);
-            return _productDal.GetList();
+            var products = _mapper.Map<List<Product>>(_productDal.GetList());
+            return products;
         }
 
         public Product GeyById(int id)
